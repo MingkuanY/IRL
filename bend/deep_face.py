6783f894d97fa0f1321b2a87bed0735b8e6af379
Pranav Tadepalli <orca.pranav@gmail.com>
2024-01-26 21:42:11 -0500

    Update deep_face.py

--- bend/deep_face.py
+++ bend/deep_face.py
@@ -1,2 +1,15 @@
 from deepface import DeepFace
-result = DeepFace.verify(img1_path = "model_face/Pranav_Tadepalli.jpg", img2_path = "pranav_seen.jpg")
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'retinaface', 
+  'mediapipe',
+  'yolov8',
+  'yunet',
+  'fastmtcnn',
+]
+
+result = DeepFace.verify(img1_path = "model_faces/Pranav_Tadepalli.png", img2_path = "pranav_seen.jpg",detector_backend = backends[4])
+print(result)
