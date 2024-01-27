29d9d79b115a9cf2b39e3ac09737024f923720df
huangkatherine7 <116393413+huangkatherine7@users.noreply.github.com>
2024-01-27 17:46:21 -0500

    still trying to build shader

--- graphics/script.py
+++ graphics/script.py
@@ -8,7 +8,7 @@ Y_CONS = 1514
 
 
 def render_text_on_image(lines, font_path, output_path):
-    image = Image.new("RGBA", (X_CONS, Y_CONS), (2, 2, 2, 255))  # Dark blue background
+    image = Image.new("RGBA", (X_CONS, Y_CONS), (3, 3, 3, 255))  # Dark blue background
     draw = ImageDraw.Draw(image)
 
     font_name = ImageFont.truetype(font_path, 60)
@@ -17,24 +17,24 @@ def render_text_on_image(lines, font_path, output_path):
 
     for line in lines:
         content = line.strip().split(',')
-        if len(content) == 10:
-            name, occupation, interests, q,w,e,r,t, x, y = content
-            x, y = float(x), float(y)
-            x*=X_CONS
-            y*=Y_CONS
+        if len(content) == 5:
+            name, occupation, interests, x, y = content
+            x, y = int(x), int(y)
 
             draw.text((x, y), name, font=font_name, fill=(94, 255, 124), align="right")
             draw.text((x, y + 60), occupation, font=font_occupation, fill=(45, 227, 227), align="right")
             draw.text((x, y + 100), interests, font=font_interests, fill=(45, 227, 227), align="right")
         else:
             print("Error: Invalid input format in the text file.")
+        
+    
     
    
     image.save(output_path) #replace result with image to revert
 
 if __name__ == "__main__":
     font_path = "SourceSansPro-Semibold.ttf"
-    input_text_path = "output.csv"
+    input_text_path = "data.txt"
     output_path = "output.png"
 
     cv2.namedWindow('Video Stream', cv2.WINDOW_NORMAL)
@@ -43,7 +43,6 @@ if __name__ == "__main__":
     while True:
         with open(input_text_path, "r") as input_file:
             lines = input_file.readlines()
-        line = lines[1] if len(lines) > 1 else ""
 
         render_text_on_image(lines, font_path, output_path)
         frame = cv2.imread(output_path)
