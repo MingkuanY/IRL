bd2606c4ec7174c67e5c4f6b084f45dd8a53900a
Pranav Tadepalli <orca.pranav@gmail.com>
2024-01-27 22:12:33 -0500

    ai

--- graphics/script.py
+++ graphics/script.py
@@ -1,44 +1,78 @@
 from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
-
+import random
+import time
 
 X_CONS = 3024
 Y_CONS = 1514
+recent_name = " "
+animation_time = 5
+current_animation_time = 0
+current_text_animation_time = 0
+image_orig = Image.new("RGBA", (X_CONS, Y_CONS), (3, 3, 3, 255))  # Dark blue background
 
+star = Image.open("starry1.png")
+def determine_animation(name):
+    if name == "Katherine_Huang":
+        return True
+    else:
+        return False
 
 def render_text_on_image(lines, font_path, output_path):
-    image = Image.new("RGBA", (X_CONS, Y_CONS), (3, 3, 3, 255))  # Dark blue background
+    global recent_name, animation_time, current_animation_time, current_text_animation_time, star, image_orig
+    image = image_orig.copy() # Dark blue background
+    
     draw = ImageDraw.Draw(image)
 
-    font_name = ImageFont.truetype(font_path, 60)
+    font_name = ImageFont.truetype(font_path_blade, 60)
     font_occupation = ImageFont.truetype(font_path, 40)
     font_interests = ImageFont.truetype(font_path, 30)
 
     for line in lines:
         content = line.strip().split(',')
-        print(len(content))
+        print(random.randint(0,20))
 
         if len(content) == 12:
-            name, occupation, interests, q,w,e,r,t,a,s, x, y = content
-            x, y = float(x), float(y)
-            x*=X_CONS
-            y*=Y_CONS
+            name, first, last, hometown,team_role,position,interests,organization,contact,image_link, x, y = content
+            x_real, y_real = float(x), float(y)
+            x_real*=X_CONS
+            y_real*=Y_CONS
+            x,y = 300,300
+            if (name!=recent_name):
+                current_text_animation_time = 0
+                if determine_animation(name):
+                    current_animation_time = animation_time
+            
+            if current_animation_time > 0:
+                # draw.text((x, int(Y_CONS/2)), "MATCH", font=font_name, fill=(0, 0, 255), align="right")
+                for i in range(50):
+                    position = (random.randint(0,X_CONS), random.randint(0,Y_CONS))  # Adjust the position where you want to paste the image
+                    image.paste(star, position)
+                current_animation_time-=1
 
+            if current_text_animation_time < 150:
+                current_text_animation_time+=0.5
 
-            draw.text((x, y), name, font=font_name, fill=(94, 255, 124), align="right")
-            draw.text((x, y + 60), occupation, font=font_occupation, fill=(45, 227, 227), align="right")
-            draw.text((x, y + 100), interests, font=font_interests, fill=(45, 227, 227), align="right")
+            recent_name = name
+
+            draw.text((x_real, y_real), (first+"\n"+last), font=font_name, fill=(75, 75, 75), align="right")
+            draw.text((x, y), (first+" "+last)[:min((150,int(current_text_animation_time)))], font=font_name, fill=(94, 255, 124), align="right")
+            draw.text((x, y + 60), hometown[:min((150,int(current_text_animation_time)))], font=font_occupation, fill=(45, 227, 227), align="right")
+            draw.text((x, y + 100), interests[:min((150,int(current_text_animation_time)))], font=font_interests, fill=(45, 227, 227), align="right")
+            draw.text((x, y + 140), team_role[:min((150,int(current_text_animation_time)))], font=font_interests, fill=(45, 227, 227), align="right")
+
         else:
             print("Error: Invalid input format in the text file.")
         
     
     
    
-    image.save(output_path) #replace result with image to revert
+    return np.array(image)
 
 if __name__ == "__main__":
-    font_path = "SourceSansPro-Semibold.ttf"
+    font_path = "blah.ttf"
+    font_path_blade = "blade.ttf"
     input_text_path = "output.csv"
     output_path = "output.png"
 
@@ -46,20 +80,26 @@ if __name__ == "__main__":
     cv2.setWindowProperty('Video Stream', cv2.WND_PROP_FULLSCREEN, cv2.WND_PROP_FULLSCREEN)
 
     while True:
+        print("FRAME")
+        currtime = time.time()
         with open(input_text_path, "r") as input_file:
             lines = input_file.readlines()
 
+        print(time.time()-currtime)
         line = lines[1] if len(lines) > 1 else ""
-        render_text_on_image([line], font_path, output_path)
-        frame = cv2.imread(output_path)
+        
+        # frame = cv2.imread(output_path)
+        frame = render_text_on_image([line], font_path, output_path)
+        print(time.time()-currtime)
 
         # Apply horizontal flipping to mirror the output
         mirrored_frame = cv2.flip(frame, 1)
 
         cv2.imshow('Video Stream', mirrored_frame)
 
-        key = cv2.waitKey(500)
+        key = cv2.waitKey(1)
         if key == 27:  # Press 'Esc' to exit
             break
+        print(time.time()-currtime)
 
     cv2.destroyAllWindows()
